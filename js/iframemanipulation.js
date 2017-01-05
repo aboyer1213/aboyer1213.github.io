727814bab6008f85c94cbfde197f9fc9801e5b0f
aboyer1213 <aboyer1213@gmail.com>
2017-01-05 15:47:01 -0500

    first commit

---
+++ js/iframemanipulation.js
@@ -0,0 +1,17 @@
+function chnga() {
+	document.getElementById("acrylic").style.background ="#0078a4";
+	document.getElementById("watercolor").style.background="#1c5379";
+	document.getElementById("mm").style.background="#1c5379";
+}
+
+function chngw() {
+	document.getElementById("acrylic").style.background ="#1c5379";
+	document.getElementById("watercolor").style.background="#0078a4";
+	document.getElementById("mm").style.background="#1c5379";
+}
+
+function chngm() {
+	document.getElementById("acrylic").style.background ="#1c5379";
+	document.getElementById("watercolor").style.background="#1c5379";
+	document.getElementById("mm").style.background="#0078a4";
+}
